3301c01bffc36714e40ec34fcd4487c90bb6ecd4
Pablo Córdoba <pablocordoba11@gmail.com>
2017-04-13 23:02:23 -0300

    Database Configuration and initials models

---
+++ api/controllers/TipoTratamientoController.js
@@ -0,0 +1,11 @@
+/**
+ * TipoTratamientoController
+ *
+ * @description :: Server-side logic for managing tipotratamientoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
+  /*localDiskDb: {
     adapter: 'sails-disk'
-  },
+  },*/
 
   /***************************************************************************
   *                                                                          *
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  localMongoDB: {
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    //user: 'username', //optional
+    //password: 'password', //optional
+    database: 'example_data_base' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
